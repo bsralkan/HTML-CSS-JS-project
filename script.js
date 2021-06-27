2e9265151ab55874f126f3347f0185a860bd3f51
bsralkan <bsr.alkan@gmail.com>
2021-06-27 15:58:05 +0300

    slider created

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 5000);
